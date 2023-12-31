c32745c2f3d378d5d3f0bff06347c05e7f0cf6b4
Andrey <cust89@mail.ru>
2023-12-31 12:13:16 +0500

    seminar1

--- src/main/java/Programm.java
+++ src/main/java/Programm.java
@@ -1,6 +1,15 @@
 import com.google.gson.GsonBuilder;
 import com.google.gson.Gson;
 
+/**
+ * 1. Создайте новый Maven или Gradle проект, следуя инструкциям из блока 1 или блока 2.
+ * 2. Добавьте зависимости org.apache.commons:commons-lang3:3.12.0 и com.google.code.gson:gson:2.8.6.
+ * 3. Создайте класс Person с полями firstName, lastName и age.
+ * 4. Используйте библиотеку commons-lang3 для генерации методов toString, equals и hashCode.
+ * 5. Используйте библиотеку gson для сериализации и десериализации объектов класса Person в формат JSON.
+ *
+ */
+
 public class Programm {
     public static void main( String[] args )
     {
@@ -18,7 +27,7 @@ public class Programm {
         Person personGson = gson.fromJson(gsonPerson, Person.class);
         System.out.println(personGson);
 
-        System.out.println("person hashCode = " + personGson.hashCode());
+        System.out.println("hashCode = " + personGson.hashCode());
 
     }
 }
